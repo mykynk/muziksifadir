4368eafc471dd020471934932c343a3760467d9a
Muhammed Yusuf Kaynak <mykynk1@gmail.com>
2020-11-13 11:57:51 +0300

    amd

--- lib/admin/views/hakkinda/admin_hakkinda.dart
+++ lib/admin/views/hakkinda/admin_hakkinda.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:muziksifadir/models/hakkinda_model/hakkinda_model.dart';
+import 'package:muziksifadir/models/hakkinda_model/ssk_model.dart';
 import 'package:muziksifadir/viewmodel/admin_model.dart';
 import 'package:muziksifadir/viewmodel/sayfalar_model.dart';
 import 'package:provider/provider.dart';
@@ -37,11 +38,69 @@ class _AdminHakkindaState extends State<AdminHakkinda> {
                   onTap: ()=>uploadImage(),
                     child: Image.network(hakkinda.photo_url),
                   ),
-                )
+                ),
+                ..._paragrafList(context)
               ],
             ),
         )
         : Container();
+  }
+
+  List<Widget> _paragrafList(BuildContext context) {
+    var paragraflar = List<Widget>();
+
+    for (var i = 0; i < hakkinda.paragraflar.length; i++) {
+      paragraflar.add(Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: TextFormField(initialValue :hakkinda.paragraflar[i] + "\n",maxLines: 5,minLines: 1,),
+      ));
+    }
+
+    return paragraflar;
+  }
+
+  List<Widget> _akademisyenList(BuildContext context) {
+    var akademisyenler = List<Widget>();
+    akademisyenler.add(Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Text(
+        "\nBirlikte Çalıştığı Akademisyenler:",
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+    ));
+    for (var i = 0; i < hakkinda.akademisyenler.length; i++) {
+      akademisyenler.add(Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Text(hakkinda.akademisyenler[i]),
+      ));
+    }
+
+    return akademisyenler;
+  }
+
+  List<Widget> _sskList(BuildContext context) {
+    var akademisyenler = List<Widget>();
+    akademisyenler.add(Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Text(
+        "\nBAŞLICA SEMİNER-SEMPOZYUM-KONSER:",
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+    ));
+    for (var i = 0; i < hakkinda.ssk.length; i++) {
+      SSK oAnkiSSK = SSK.fromMap(hakkinda.ssk[i]);
+      akademisyenler.add(Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: RichText(
+            text: TextSpan(children: [
+              TextSpan(text: oAnkiSSK.isim+ "\n"),
+              TextSpan(text: oAnkiSSK.yer + "\n"),
+            ],style: TextStyle(color:Colors.black)),
+          ),
+      ));
+    }
+
+    return akademisyenler;
   }
 
   hakkindaGetir() async {
